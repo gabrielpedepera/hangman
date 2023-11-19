81fa9dbbb2e0cd1fcc69d4e42269a169605e01eb
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-11-19 14:29:39 +0000

    Formatting...

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,12 +1,10 @@
 defmodule Hangman.Impl.Game do
-  
   @type t :: %__MODULE__{
-    turns_left: integer,
-    game_state: Hangman.state,
-    letters: list(String.t),
-    used: MapSet.t(String.t)
- 
-  }
+          turns_left: integer,
+          game_state: Hangman.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
   defstruct(
     turns_left: 7,
     game_state: :initializing,
