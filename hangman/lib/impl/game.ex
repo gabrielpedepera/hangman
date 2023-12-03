de05b511867ddc7a2a597a5722bd7e643bf2fe60
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-12-03 23:35:31 +0000

    Refactoring adding module Type

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,7 +1,9 @@
 defmodule Hangman.Impl.Game do
+  alias Hangman.Type
+
   @type t :: %__MODULE__{
           turns_left: integer,
-          game_state: Hangman.state(),
+          game_state: Type.state(),
           letters: list(String.t()),
           used: MapSet.t(String.t())
         }
@@ -12,10 +14,12 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
+  @spec new_game() :: Type.game()
   def new_game do
     new_game(Dictionary.random_word())
   end
 
+  @spec new_game(String.t()) :: Type.game()
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.downcase() |> String.codepoints()
