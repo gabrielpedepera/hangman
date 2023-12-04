6d184c1e1850243c7d96533eed5e38651beeb998
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-12-04 00:02:11 +0000

    Add make_move function

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -14,15 +14,29 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
-  @spec new_game() :: Type.game()
+  @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word())
   end
 
-  @spec new_game(String.t()) :: Type.game()
+  @spec new_game(String.t()) :: t
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.downcase() |> String.codepoints()
+    }
+  end
+
+  @spec make_move(t, String.t()) :: {t, Type.tally()}
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
 end
